37cba0f2bc3c532bcbd674c1b438e6685b7f8c43
Kimserey Lam <kimserey.lam@gmail.com>
2016-09-23 08:08:22 +0100

    vertical bar

--- WebViewTest/BarPage.cs
+++ WebViewTest/BarPage.cs
@@ -8,8 +8,8 @@ namespace WebViewTest
 	{
 		public class Data
 		{
-			public double[] X { get; set; }
-			public string[] Y { get; set; }
+			public string[] X { get; set; }
+			public double[] Y { get; set; }
 			public string Type { get; set; }
 			public string Orientation { get; set; }
 		}
@@ -20,14 +20,14 @@ namespace WebViewTest
 
 			var data = new[] {
 				new Data {
-					X = new[] { 24.0, 30.0, 34.0 },
-					Y = new[] { "Dog", "Cat", "Fish" },
+					X = new[] { "Dog", "Cat", "Fish" },
+					Y = new[] { 24.0, 30.0, 34.0 },
 					Orientation = "v",
 					Type = "bar"
 				},
 				new Data {
-					X = new[] { 10.0, 30.0, 60.0 },
-					Y = new[] { "Dog", "Cat", "Fish" },
+					X = new[] { "Dog", "Cat", "Fish" },
+					Y = new[] { 10.0, 30.0, 60.0 },
 					Orientation = "v",
 					Type = "bar"
 				}
@@ -50,7 +50,8 @@ namespace WebViewTest
 		                .append('div')
 		                .style({
 		                    width: '100%',
-		                    'margin-top': '5%'
+		                    'margin-top': '5%', 
+							height: '90%'
 		                })
 		                .node();
 
